f8934582ad79a202275eb443086f4f8f189f8ee4
Mai <garcia.ton94@gmail.com>
2023-07-05 20:20:30 -0400

    completed lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,19 @@
 import requests
 import json
 
-class GetRequester:
 
+class GetRequester:
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        content_list = []
+        contents = json.loads(self.get_response_body())
+        for content in contents:
+            content_list.append(content)
+
+        return content_list
